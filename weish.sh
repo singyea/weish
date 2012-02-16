94043a1bf16bc5406761693385172701c5001b2c
singyea <singyea@163.com>
2012-02-16 20:38:55 +0800

    第一个可用版本,先用浏览器登录手机微博,然后获取到url中的GSID填入脚本的GSID，可以浏览微博，仅限第一页。

--- weish.sh
+++ weish.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-echo hello
+
+WEIBO_LOGIN_PAGE="http://weibo.cn/?gsid="
+WEIBO_LOGIN_PAGE_SID=test
+
+CURL=curl
+
+${CURL} ${WEIBO_LOGIN_PAGE}${WEIBO_SID} 2>/dev/null  | sed 's/>/>\n/g' | sed '1,102d'| sed ':a;N;s/\n/\t/;ba;'|sed 's/<    div class=\"c\"/\r\n\r\n<div class=\"c\"/g' | sed -e :a -e 's/<[^>]*>//g;/</N;//ba' |  sed 's/[[:space:]]    //g' | sed  's/&nbsp;//g'
+
